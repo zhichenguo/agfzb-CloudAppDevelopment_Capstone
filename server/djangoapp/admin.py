4a3da674a16b8e199afd90dc5b02717771d72bee
John Guo <john@visimo.ai>
2022-07-25 11:10:26 -0400

    Car models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,59 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake, CarModel
 
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInlineInline(admin.TabularInline):
+    model = CarModel
+    fields = ["name", "dealer_id", "type", "year"]
+    can_delete = True
+    show_change_link = True
+
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = [
+        "uid",
+        "car_make",
+        "name",
+        "dealer_id",
+        "type",
+        "year",
+    ]
+    list_filter = ["type"]
+    raw_id_fields = ["car_make"]
+    search_fields = [
+        "uid",
+        "car_make__uid",
+        "car_make__name",
+        "type",
+        "name",
+    ]
+
+    def get_car_make_name(self, obj):
+        return obj.car_make.name
+
+    get_car_make_name.short_description = "car_make"
+    get_car_make_name.admin_order_field = "car_make__name"
+
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = [
+        "uid",
+        "name",
+        "description",
+    ]
+    search_fields = [
+        "uid",
+        "name",
+        "description",
+    ]
+    inlines = [CarModelInlineInline]
+
 
 # Register models here
+admin.site.register(CarModel, CarModelAdmin)
+admin.site.register(CarMake, CarMakeAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,6 +9,24 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    uid = models.AutoField(
+        primary_key=True,
+        editable=False,
+        unique=True,
+        verbose_name="Car Unique ID",
+    )
+    name =  models.CharField(
+        null=False,
+        max_length=256,
+    )
+    description = models.CharField(
+        null=False,
+        max_length=1024,
+    )
+
+    def __str__(self):
+        return self.name
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -19,6 +37,36 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    uid = models.AutoField(
+        primary_key=True,
+        editable=False,
+        unique=True,
+        verbose_name="Car Unique ID",
+    )
+    car_make = models.ForeignKey(
+        CarMake,
+        on_delete=models.CASCADE,
+    )
+    name =  models.CharField(
+        null=False,
+        max_length=255,
+    )
+    dealer_id = models.IntegerField()
+    type = models.CharField(
+        null=False,
+        max_length=20,
+        choices=[
+            ('sedan', 'Sedan'),
+            ('suv', 'SUV'),
+            ('wagon', 'WAGON'),
+            ('pickup truck', 'Pickup Truck')
+        ]
+    )
+    year = models.DateField(null=True)
+
+    def __str__(self):
+        return self.name
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
